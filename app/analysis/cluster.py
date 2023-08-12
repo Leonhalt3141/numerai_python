d361d264136dcb5ae238b24ff9c04a2082e7d9ef
Kentaro Kuwata <kuwaken0925@gmail.com>
2023-08-12 16:01:43 -0700

    Add cluster script

--- app/analysis/cluster.py
+++ app/analysis/cluster.py
@@ -1,5 +1,8 @@
 ##
 import pandas as pd
+import xgboost as xgb
+from joblib import load
+from matplotlib import pyplot as plt
 
 ##
 train_df = pd.read_parquet("train.parquet").head(100)
@@ -9,4 +12,12 @@ feature_columns = [c for c in train_df.columns if "feature" in c]
 print(f"Feature columns: {len(feature_columns)}")
 target_columns = [c for c in train_df.columns if "target" in c]
 print(f"Target columns {len(target_columns)}")
+##
+model_path = "model.lgb.fold_1.pkl"
+model = load(model_path)
+##
+fig, ax = plt.subplots(figsize=(10, 20))
+xgb.plot_importance(model, ax=ax, importance_type="gain")
+plt.show()
+
 ##
